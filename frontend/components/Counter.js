4f838b6c3e3fdde62316b57ce34454acc9c9b3f2
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-12-19 20:55:16 -0500

    div adjustments from hardcoded

--- frontend/components/Counter.js
+++ frontend/components/Counter.js
@@ -50,7 +50,7 @@ import { useState } from 'react';
 
 export default function Counter() {
 
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
   
   /* STEP 1 */
 
@@ -67,14 +67,14 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color:  count % 2 === 0 ? 'royalblue' : 'crimson'
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
